dbfc4ae18266dd6ac672011425099b9e86e835b2
kim-tang2 <rlaxogns4504@gmail.com>
2020-10-02 22:28:51 +0900

    Feature px_page UI

--- lib/screens/px_page.dart
+++ lib/screens/px_page.dart
@@ -16,23 +16,92 @@ class _PxPageState extends State<PxPage> {
         child: FutureBuilder<List<PX>>(
           future: Provider.of<GetDataProvider>(context, listen: false).getPX(),
           builder: (context, snapshot) {
-            print(snapshot.data);
-            if (snapshot.data == null) {
-              return Container(
-                child: Center(
+            switch(snapshot.connectionState){
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
                   child: CircularProgressIndicator(),
-                ),
-              );
-            } else {
-              return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    title: Text(snapshot.data[index].name),
-                  );
-                },
-              );
+                );
+              default:
+                final product = snapshot.data;
+                return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                      height: 150,
+                      width: 344,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(14),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 1.5,
+                              blurRadius: 5,
+                              offset: Offset(1, 2),
+                            )
+                          ]),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 40),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                Container(
+                                  width: 100,
+                                  height: 100,
+                                  child: Image.network(product[index].thumbnail),
+                                ),
+
+                                Flexible(
+                                  child: Text(
+                                    product[index].name,
+                                    style: TextStyle(
+                                      color: Colors.grey[800],
+                                      fontFamily: 'NotoSans-Bold',
+                                      fontSize: 15.0,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+//                    return ListTile(
+//                      leading: CircleAvatar(
+//                        backgroundImage: NetworkImage(product[index].thumbnail),
+//                      ),
+//                      title: Text(product[index].name),
+//                    );
+                  },
+                );
             }
+//
+//            print(snapshot.data);
+//            if (snapshot.data == null) {
+//              return Container(
+//                child: Center(
+//                  child: CircularProgressIndicator(),
+//                ),
+//              );
+//            } else {
+//              return ListView.builder(
+//                itemCount: snapshot.data.length,
+//                itemBuilder: (BuildContext context, int index) {
+//                  return ListTile(
+//                    leading: CircleAvatar(
+//                      backgroundImage: NetworkImage(snapshot.data[index].thumbnail),
+//                    ),
+//                    title: Text(snapshot.data[index].name),
+//                  );
+//                },
+//              );
+//            }
           },
         ),
       ),
